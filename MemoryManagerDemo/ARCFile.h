ac05bc2bae3d07a564c77e18ca1b3d214113bf01
tenyears <comexie@yahoo.com>
2018-09-30 15:20:35 +0800

    add Arc file

---
+++ MemoryManagerDemo/ARCFile.h
@@ -0,0 +1,17 @@
+//
+//  ARCFile.h
+//  MemoryManagerDemo
+//
+//  Created by yuqian on 2018/9/30.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ARCFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MemoryManagerDemo/ARCFile.m
@@ -0,0 +1,13 @@
+//
+//  ARCFile.m
+//  MemoryManagerDemo
+//
+//  Created by yuqian on 2018/9/30.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import "ARCFile.h"
+
+@implementation ARCFile
+
+@end
